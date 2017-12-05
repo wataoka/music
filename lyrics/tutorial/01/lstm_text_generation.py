3c4581aba3a7aa65207d3cc804a351a2b847749e
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2017-12-05 16:59:02 +0900

    作詞チュートリアルの作成

---
+++ lyrics/tutorial/01/lstm_text_generation.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+from keras.utils.data_utils import get_file
+import numpy as np
+import numpy as np
+import random
+import sys
+
+path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open(path).read().lower()
+print()
+print("-------------------------")
+print()
+print(text[0:10000])
