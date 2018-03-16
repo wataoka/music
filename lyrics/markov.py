783388d1da090575644e68619ce1ab0904babfb8
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2018-03-16 11:58:41 +0900

    markov.py完成

--- lyrics/markov.py
+++ lyrics/markov.py
@@ -1,44 +1,71 @@
 import random
 import MeCab
-import pandas as pd
+import glob
 
-def csv2txt(filename):
-
-    df = pd.read_csv(filename)
-    lyric = df['lyric']
-    lyric.to_csv('test.txt')
 
 def wakati(text):
+
     t = MeCab.Tagger("-Owakati")
     m = t.parse(text)
     result = m.rstrip(" \n").split(" ")
     return result
 
-if __name__ == "__main__":
-    csv2txt('./data/01/#嘲笑ポラロイド.csv')
-    filename = "test.txt"
-    src = open(filename ,"r").read()
-    wordlist = wakati(src)
 
-    # マルコフ連鎖用のテーブルを作成する
+
+def markov():
+
+    file_list = glob.glob('./data/txt/*.txt')
     markov = {}
-    w1 = ""
-    w2 = ""
-    for word in wordlist:
-        if w1 and w2:
-            if (w1, w2) not in markov:
-                markov[(w1, w2)] = []
-            markov[(w1, w2)].append(word)
-        w1, w2 = w2 ,word
-
+    for filename in file_list:
+        src = open(filename, 'r').read()
+        wordlist = wakati(src)
+        w1 = ""
+        w2 = ""
+        for word in wordlist:
+            if w1 and w2:
+                if(w1, w2) not in markov:
+                    markov[(w1, w2)] = []
+                markov[(w1, w2)].append(word)
+            w1, w2 = w2, word
 
     count = 0
     sentence = ""
     w1, w2 = random.choice(list(markov.keys()))
-    while count < len(wordlist):
+    while count < 300:
         tmp = random.choice(markov[(w1, w2)])
         sentence += tmp
         w1, w2 = w2, tmp
         count += 1
 
     print(sentence)
+
+
+
+if __name__ == "__main__":
+    markov()
+    # filename = "./data/txt/#嘲笑ポラロイド.txt"
+    # src = open(filename ,"r").read()
+    # wordlist = wakati(src)
+
+    # # マルコフ連鎖用のテーブルを作成する
+    # markov = {}
+    # w1 = ""
+    # w2 = ""
+    # for word in wordlist:
+    #     if w1 and w2:
+    #         if (w1, w2) not in markov:
+    #             markov[(w1, w2)] = []
+    #         markov[(w1, w2)].append(word)
+    #     w1, w2 = w2 ,word
+
+
+    # count = 0
+    # sentence = ""
+    # w1, w2 = random.choice(list(markov.keys()))
+    # while count < len(wordlist):
+    #     tmp = random.choice(markov[(w1, w2)])
+    #     sentence += tmp
+    #     w1, w2 = w2, tmp
+    #     count += 1
+
+    # print(sentence)
