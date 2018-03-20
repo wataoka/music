40b29a615bf60f7e97779e5194272eae8726f97d
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2018-03-20 18:47:00 +0900

    lyricsNet.pyを日本語に対応

--- lyrics/lyricsNet.py
+++ lyrics/lyricsNet.py
@@ -9,6 +9,8 @@ from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
+import MeCab
+import glob
 import numpy as np
 import random
 import sys
@@ -22,15 +24,15 @@ class LyricsNet():
     LyricsNet with LSTM
     """
 
-    def __init__(self, text, chars, char_indices, indices_char):
+    def __init__(self, maxlen, step, text, chars, char_indices, indices_char):
 
+        self.maxlen = maxlen
+        self.step = step
         self.text = text
         self.chars = chars
         self.char_indices = char_indices
         self.indices_char = indices_char
 
-        self.maxlen = 40
-        self.step = 3
 
 
     def createModel(self, input_shape, n_class):
@@ -60,25 +62,26 @@ class LyricsNet():
         for diversity in [0.2, 0.5, 1.0, 1.2]:
             print('----- diversity:', diversity)
 
-            generated = ''
+            generated = []
             sentence = self.text[start_index: start_index + self.maxlen]
-            generated += sentence
-            print('----- Generating with seed: "' + sentence + '"')
-            sys.stdout.write(generated)
+            generated = sentence
+            print('----- Generating with seed: "' + ''.join(sentence) + '"')
+            sys.stdout.write(''.join(generated))
 
             for i in range(400):
                 x_pred = np.zeros((1, self.maxlen, len(self.chars)))
-                for t, self.char in enumerate(sentence):
-                    x_pred[0, t, self.char_indices[self.char]] = 1.
+                for t, word in enumerate(sentence):
+                    x_pred[0, t, self.char_indices[word]] = 1.
 
+                # 与えられたsentenceから次の単語を予想
                 preds = model.predict(x_pred, verbose=0)[0]
                 next_index = self.sample(preds, diversity)
-                next_char = self.indices_char[next_index]
+                next_word = self.indices_char[next_index]
 
-                generated += next_char
-                sentence = sentence[1:] + next_char
+                sentence.append(next_word)
+                sentence.pop(0)
 
-                sys.stdout.write(next_char)
+                sys.stdout.write(next_word)
                 sys.stdout.flush()
             print()
 
@@ -95,20 +98,32 @@ class LyricsNet():
         return np.argmax(probas)
 
 
+def wakati(text):
+
+    t = MeCab.Tagger("-Owakati")
+    m = t.parse(text)
+    result = m.rstrip(" \n").split(" ")
+    return result
+
 
 def load_data():
 
-    path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
-    with io.open(path, encoding='utf-8') as f:
-           text = f.read().lower()
+    file_list = glob.glob('./data/txt/*.txt')
+    text = []
+    for file in file_list:
+        src = open(file, 'r').read()
+        wordlist = wakati(src)
+        for word in wordlist:
+            text.append(word)
+
 
     chars = sorted(list(set(text)))
     print('total chars:', len(chars))
     char_indices = dict((c, i) for i, c in enumerate(chars))
     indices_char = dict((i, c) for i, c in enumerate(chars))
 
-    maxlen = 40
-    step = 3
+    maxlen = 5
+    step = 2
     sentences = []
     next_chars = []
     for i in range(0, len(text) - maxlen, step):
@@ -124,17 +139,17 @@ def load_data():
             x[i, t, char_indices[char]] = 1
         y[i, char_indices[next_chars[i]]] = 1
 
-    return (x, y), (text, chars, char_indices, indices_char)
+    return (x, y), (maxlen, step), (text, chars, char_indices, indices_char)
 
 
 
 if __name__ == "__main__":
 
     # データの作成
-    (x, y), (text, chars, char_indices, indices_char) = load_data()
+    (x, y), (maxlen, step), (text, chars, char_indices, indices_char) = load_data()
 
     # LyricsNet生成
-    LyricsNet = LyricsNet(text, chars, char_indices, indices_char)
+    LyricsNet = LyricsNet(maxlen, step, text, chars, char_indices, indices_char)
 
     # モデルを定義
     model = LyricsNet.createModel(x.shape[1:], y.shape[1])
